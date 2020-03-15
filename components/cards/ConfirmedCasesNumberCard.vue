33119cc80a0a265ecd16ca4fc9abd44d6691ed9d
tatsuya1970 <tatsuya.takemura@gmail.com>
2020-03-15 16:21:03 +0900

    Update ConfirmedCasesNumberCard.vue

--- components/cards/ConfirmedCasesNumberCard.vue
+++ components/cards/ConfirmedCasesNumberCard.vue
@@ -7,7 +7,7 @@
       :chart-data="patientsGraph"
       :date="Data.patients.date"
       :unit="$t('人')"
-      :url="'https://catalog.data.metro.tokyo.lg.jp/dataset/t000010d0000000068'"
+      :url="'https://www.pref.hiroshima.lg.jp/soshiki/57/bukan-coronavirus.html'"
     />
   </v-col>
 </template>
